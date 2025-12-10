010d9925de8b2130bb019a6befd5860627bbbb2a
Franco <f.cancinodiaz@gmail.com>
2025-12-10 04:01:36 -0300

    Se cran archivos html, css y javascript

---
+++ assets/js/scriptactividad11.js
@@ -0,0 +1,12 @@
+let numero1 = Number(prompt("Ingresa el primer número:"));
+let numero2 = Number(prompt("Ingresa el segundo número"));
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2; 
+
+alert('La suma es: ' + suma);
+alert('La resta es: ' + resta);
+alert('La multiplicación es: ' +multiplicacion);
+alert('La división es: ' + division);
